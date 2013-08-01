b7746e48e1707add9483a668dafef7ad3854f7ef
richardtow <richard.tow@gmail.com>
2013-08-01 16:57:01 -0700

    Initialize repository, 1st Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f5e2dc5d71a9769b0226c56e52066a526cad37479c36565f81de00a31a01252bc4e7ed656faf52e1eace1a9c44ce39f895294b2b3e87f7476f28acdfed6d47db'
